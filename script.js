faf6e75319f62dd283224b7878f229f8e4a5e25a
maokana <165888363+maokana@users.noreply.github.com>
2026-04-12 21:26:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 async function send() {
   const input = document.getElementById("input").value;
 
-  const res = await fetch("https://あなたのworker.workers.dev", {
+  const res = await fetch("https://fancy-salad-35ac.kanachenliebe.workers.dev
+", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
